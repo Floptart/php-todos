93e12f6c3bb059020fa4b61f44184b423baa5c18
Floptart <floptart@gmail.com>
2017-08-24 18:44:00 +0000

    create routes, routing, and refactoring old code.

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = require 'config.php';
-require 'database/QueryBuilder.php';
-require 'database/Connection.php';
-
-
-return new QueryBuilder(Connection::make($config['database']));
